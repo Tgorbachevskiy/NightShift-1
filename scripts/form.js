f771ebe2b348e599688924edeb5faf1e13d8e67b
Maureen Kariuki <kmaureen44@gmail.com>
2019-10-15 19:22:14 -0700

    Changed the insertAfter to insert reference portion after the ID of the reference instead of the h6. This is to avoid the reference portion being added to the policy portion

--- scripts/form.js
+++ scripts/form.js
@@ -1,7 +1,7 @@
 //Add a new reference
-let referenceForm = '<div class="ref"><div class="input-field col s6"><label for="refName">Reference First Name</label><input type="text" class="form-control" id="refFirstName"></div><div class="input-field col s6"><label for="refName">Reference Last Name</label><input type="text" class="form-control" id="refLastName"></div><div class="input-field col s12"><label for="refphone">Phone #</label><input type="number" class="form-control" id="refphone1"></div><div class="input-field col s12"><label for="relation">Relation</label><input type="text" class="form-control" id="relation1"></div></div><br>';
+let referenceForm = '<div class="ref"><div class="input-field col s6"><label for="refFirstName">Reference First Name</label><input type="text" class="form-control" id="refFirstName"></div><div class="input-field col s6"><label for="refLastName">Reference Last Name</label><input type="text" class="form-control" id="refLastName"></div><div class="input-field col s12"><label for="refphone1">Phone #</label><input type="number" class="form-control" id="refphone1"></div><div class="input-field col s12"><label for="relation1">Relation</label><input type="text" class="form-control" id="relation1"></div></div><br>';
 $("#newref").click(function(){
-  $(referenceForm).insertAfter("h6");
+  $(referenceForm).insertAfter("#referenceLegend");
 });
 
   $(document).ready(function(){
