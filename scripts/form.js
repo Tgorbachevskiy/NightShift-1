5d35f2a690b75c690019ce00fbf5e984e86c2cbc
Ngadomskiy09 <ngadomskiy09@gmail.com>
2019-10-28 18:38:45 -0700

    added a section to pick hours if user selected weekends for availab

--- scripts/form.js
+++ scripts/form.js
@@ -26,47 +26,53 @@ $("#noPref").click(function(){
   $("#rolePref").toggle();
 });
 
-/*Show or Hide other textarea function
-* for interests
- */
-$("#other-interest").hide();
-$("#other-interests").change(function () {
-    if ($("#other-interests").is(":checked")) {
-        $("#other-interest").show();
-    }
-    else {
-        $("#other-interest").hide();
-    }
+//Initialize select
+$(document).ready(function () {
+  $('select').formSelect();
+});
+//Initialize collapsible for references
+$(document).ready(function(){
+  $('.collapsible').collapsible();
 });
 
 
-/*Show or Hide other textarea function
-* for how did you hear about us
- */
-$(function () {
-    $("#other-block").hide();
-    $("#howdidyouhear").change(function () {
-        var heardAbout = $(this).val();
-        if(heardAbout === "other") {
-            $("#other-block").show();
-        }
-        else {
-            $("#other-block").hide();
-        }
 
-    });
+/*
+    add a mouse over for pulsate on the submit button,
+    remove it on mouse out
+ */
+$("#submit").mouseover(function () {
+  $("button").addClass("pulse");
 });
+$("#submit").mouseout(function () {
+  $("button").removeClass("pulse");
+});
+
+$(document).ready(function () {
+  $('.datepicker').datepicker();
+});
+
 
 // function to display message if user selects no from the
 // consent radio buttons
 function func() {
-    var conN = document.getElementById("no").checked;
-    var con = document.getElementById("consent");
-    if(conN == true) {
-        con.style.display = "block";
-    } else {
-        con.style.display = "none";
-    }
+  var conN = document.getElementById("no").checked;
+  var con = document.getElementById("consent");
+  if(conN == true) {
+    con.style.display = "block";
+  } else {
+    con.style.display = "none";
+  }
+}
+
+// toggles the hours that pop up for availability if weekend is selected
+function time() {
+  var chb = document.getElementById("weekends");
+  if(chb.checked) {
+    document.getElementById("availability").style.display = "block";
+  } else {
+    document.getElementById("availability").style.display = "none";
+  }
 }
 
 
@@ -75,43 +81,45 @@ document.getElementById("volunteer-form").onsubmit = validate;
 
 function validate() {
 
-    var Valid = true;
+  var Valid = true;
 
-    var errors = document.getElementsByClassName("err");
-    for(var i = 0; i < errors.length; i++) {
-        errors[i].style.visibility = "hidden";
-    }
+  var errors = document.getElementsByClassName("err");
+  for(var i = 0; i < errors.length; i++) {
+    errors[i].style.visibility = "hidden";
+  }
 
-    // validates if a consent option was selected
-    var Y = document.getElementById("yes").checked;
-    var N = document.getElementById("no").checked;
-    if (Y == false && N == false) {
-        var errConsent = document.getElementById("err-consent");
-        errConsent.style.visibility = "visible";
-        Valid = false;
-    }
+  // validates if a consent option was selected
+  var Y = document.getElementById("yes").checked;
+  var N = document.getElementById("no").checked;
+  if (Y == false && N == false) {
+    var errConsent = document.getElementById("err-consent");
+    errConsent.style.visibility = "visible";
+    Valid = false;
+  }
 
-    var address = document.getElementById("address").value;
-    if (address == "") {
-        var errAddress = document.getElementById("err-address");
-        errAddress.style.visibility = "visible";
-        Valid = false;
-    }
+  // validates that the address is not blank
+  var address = document.getElementById("address").value;
+  if (address == "") {
+    var errAddress = document.getElementById("err-address");
+    errAddress.style.visibility = "visible";
+    Valid = false;
+  }
 
-    var city = document.getElementById("city").value;
-    if (city == "") {
-        var errCity = document.getElementById("err-city");
-        errCity.style.visibility = "visible";
-        Valid = false;
-    }
+  // validates that the city is not blank
+  var city = document.getElementById("city").value;
+  if (city == "") {
+    var errCity = document.getElementById("err-city");
+    errCity.style.visibility = "visible";
+    Valid = false;
+  }
 
-    var zip = document.getElementById("zip").value;
-    if (zip == "") {
-        var errZip = document.getElementById("err-zip");
-        errZip.style.visibility = "visible";
-        Valid = false;
-    }
+  // validates that the zip code is not blank
+  var zip = document.getElementById("zip").value;
+  if (zip == "") {
+    var errZip = document.getElementById("err-zip");
+    errZip.style.visibility = "visible";
+    Valid = false;
+  }
 
-    return Valid;
+  return Valid;
 }
-
